8f1c3cf5757b2f889104a21ccfce331f52f8b91e
Gabriel Seaver <gabrielseaver9678@gmail.com>
2022-02-28 19:08:35 -0500

    auto shooter sequence for auton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.central.AutoShooterSequence;
 import frc.robot.commands.central.CentralSystem;
 import frc.robot.commands.climber.ClimberCommand;
@@ -132,7 +133,9 @@ public class RobotContainer {
 	}
 	
 	public Command getAutonomousCommand () {
-		return new AutonDrive(swerveDrive, 0, 12, 0.2, 3, 0.01, FrameOfReference.ROBOT);
+		return new SequentialCommandGroup(
+			new AutoShooterSequence(shooter, cargoHandler, 1.5),
+			new AutonDrive(swerveDrive, 0, 12, 0.2, 3, 0.01, FrameOfReference.ROBOT));
 	}
 	
 	public void onFirstRobotEnable () {
