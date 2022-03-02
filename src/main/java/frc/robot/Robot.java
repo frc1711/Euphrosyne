0ec35620cda8993bfa82cdff6a1b71b5fabce5ff
Gabriel Seaver <gabrielseaver9678@gmail.com>
2022-03-02 18:49:24 -0500

    camera changing added to button

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.CameraSystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -21,14 +20,11 @@ public class Robot extends TimedRobot {
 	private RobotContainer m_robotContainer;
 	private boolean robotHasBeenEnabled = false;
 	
-	private CameraSystem cameraSystem;
-	
 	@Override
 	public void robotInit () {
 		// Instantiate our RobotContainer.  This will perform all our button bindings, and put our
 		// autonomous chooser on the dashboard.
 		m_robotContainer = new RobotContainer();
-		cameraSystem = new CameraSystem();
 	}
 	
 	@Override
@@ -38,7 +34,6 @@ public class Robot extends TimedRobot {
 		// and running subsystem periodic() methods.  This must be called from the robot's periodic
 		// block in order for anything in the Command-based framework to work.
 		CommandScheduler.getInstance().run();
-		cameraSystem.periodicUpdate();
 	}
 	
 	@Override
---
+++ src/main/java/frc/robot/subsystems/CameraSystem.java
@@ -0,0 +1,78 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.MjpegServer;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class CameraSystem extends SubsystemBase {
+	
+	public CameraSystem () {
+		// Initializes USB cameras
+		for (CameraOption option : CameraOption.values())
+			option.initCamera();
+		
+		// Initializes the camera server to the first given camera
+		server = CameraServer.addSwitchedCamera("Camera");
+		setCamera(CameraOption.values()[0]);
+	}
+	
+	private MjpegServer server;
+	private UsbCamera currentCamera;
+	
+	public static enum CameraOption {
+		CAMERA_1 (0),
+		CAMERA_2 (1);
+		
+		private final int dev;
+		private UsbCamera camera;
+		
+		private CameraOption (int dev) {
+			this.dev = dev;
+		}
+		
+		private void initCamera () {
+			this.camera = new UsbCamera(this.name(), dev);
+		}
+	}
+	
+	// Keeps track of the current index so we can cycle through one at a time
+	private int currentCameraOptionIndex;
+	
+	public void setCamera (CameraOption option) {
+		if (currentCamera == option.camera) return;
+		
+		// Updates the camera options index
+		currentCameraOptionIndex = getCameraOptionsIndex(option);
+		
+		// Updates the current camera
+		currentCamera = option.camera;
+		
+		// Updates the server
+		server.setSource(currentCamera);
+	}
+	
+	private int getCameraOptionsIndex (CameraOption option) {
+		CameraOption[] options = CameraOption.values();
+		for (int i = 0; i < options.length; i ++)
+			if (options[i] == option) return i;
+		return 0;
+	}
+	
+	public void nextCamera () {
+		int nextIndex = putCameraOptionsIndexInBounds(currentCameraOptionIndex + 1);
+		setCamera(CameraOption.values()[nextIndex]);
+	}
+	
+	public void previousCamera () {
+		int nextIndex = putCameraOptionsIndexInBounds(currentCameraOptionIndex - 1);
+		setCamera(CameraOption.values()[nextIndex]);
+	}
+	
+	private int putCameraOptionsIndexInBounds (int index) {
+		if (index < 0) return CameraOption.values().length - 1;
+		if (index >= CameraOption.values().length) return 0;
+		return index;
+	}
+	
+}
