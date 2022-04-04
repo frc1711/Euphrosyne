27cebb98be3da4ef47c1753e99f23986026f7c3b
Gabriel Seaver <gabrielseaver9678@gmail.com>
2022-04-04 19:48:24 -0400

    Adjusted Auton Trifecta constants.

--- src/main/java/frc/robot/commands/auton/AutoTrifecta.java
+++ src/main/java/frc/robot/commands/auton/AutoTrifecta.java
@@ -3,12 +3,14 @@ package frc.robot.commands.auton;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.auton.base.AutoDrive;
 import frc.robot.commands.auton.base.AutoDriveIntakeHandler;
+import frc.robot.commands.auton.base.AutoIntake;
 import frc.robot.commands.auton.base.AutoShooterSequence;
 import frc.robot.commands.auton.base.AutoTurn;
 import frc.robot.subsystems.CargoHandler;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Swerve;
+import frc.robot.Dashboard;
 
 public class AutoTrifecta extends SequentialCommandGroup {
 	
@@ -17,15 +19,18 @@ public class AutoTrifecta extends SequentialCommandGroup {
 			// Shoot initial ball
 			new AutoShooterSequence(shooter, cargoHandler, 0.5),
 			
-			// Get first ball
+			// Deploy the intake
+			new AutoIntake(intake, 0.5, Dashboard.INTAKE_MAX_SPEED.get()),
+			
+			// Get first balldelpo
 			new AutoDrive(swerveDrive, 300, 3),
-			new AutoTurn(swerveDrive, -28, 2.5),
+			new AutoTurn(swerveDrive, -22, 2.5),
 			new AutoDriveIntakeHandler(swerveDrive, intake, cargoHandler, 215, 3),
 			
 			// Get second ball
 			new AutoDriveIntakeHandler(swerveDrive, intake, cargoHandler, -215, 3),
-			new AutoTurn(swerveDrive, 100, 2.5),
-			new AutoDriveIntakeHandler(swerveDrive, intake, cargoHandler, 450, 3),
+			new AutoTurn(swerveDrive, 93, 2.5),
+			new AutoDriveIntakeHandler(swerveDrive, intake, cargoHandler, 465, 3),
 			
 			// Go back
 			new AutoDriveIntakeHandler(swerveDrive, intake, cargoHandler, -450, 3),
