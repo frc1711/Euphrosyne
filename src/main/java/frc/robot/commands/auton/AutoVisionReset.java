fabb6454fc38c1d65fd8775908dbfd612e1fe409
Gabriel Seaver <gabrielseaver9678@gmail.com>
2022-09-12 02:22:40 -0400

    finished AutoVisionReset, it should now actually reset the swerve's odometry

--- src/main/java/frc/robot/commands/auton/AutoVisionReset.java
+++ src/main/java/frc/robot/commands/auton/AutoVisionReset.java
@@ -1,7 +1,11 @@
 package frc.robot.commands.auton;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.team1711.swerve.util.Vector;
+import frc.team1711.swerve.util.odometry.Position;
 import frc.team1711.swerve.util.odometry.TurnManner.TurnSpeedSupplier;
 import frc.robot.subsystems.Swerve;
 import frc.robot.util.VisionHandler;
@@ -9,10 +13,10 @@ import frc.robot.util.VisionHandler;
 public class AutoVisionReset extends SequentialCommandGroup {
     
     public AutoVisionReset (Swerve swerve) {
-        super(new AutoAimAtVisionTarget(swerve));
+        super(new AutoAimAtVisionTarget(swerve), new WaitCommand(0.1), new AutoResetOdometry(swerve));
     }
     
-    public static class AutoAimAtVisionTarget extends CommandBase {
+    private static class AutoAimAtVisionTarget extends CommandBase {
         
         private static final TurnSpeedSupplier TURN_SPEED_SUPPLIER = TurnSpeedSupplier.speedWithSlowdown(0.15, 10, 0.02);
         private static final double MARGIN_OF_ERROR = 0.4; // This has to be really small for accurate limelight readings
@@ -24,7 +28,7 @@ public class AutoVisionReset extends SequentialCommandGroup {
         
         private int numFramesInMargin = 0;
         
-        public AutoAimAtVisionTarget (Swerve swerve) {
+        private AutoAimAtVisionTarget (Swerve swerve) {
             this.swerve = swerve;
             addRequirements(swerve);
         }
@@ -53,7 +57,6 @@ public class AutoVisionReset extends SequentialCommandGroup {
         @Override
         public void end (boolean interrupted) {
             swerve.stop();
-            System.out.println("\n\n\nDISTANCE FROM HUB: " + visionHandler.getDistanceFromHub() + " in\n\n\n");
         }
         
         @Override
@@ -62,4 +65,26 @@ public class AutoVisionReset extends SequentialCommandGroup {
         }
         
     }
+    
+    private static class AutoResetOdometry extends InstantCommand {
+        
+        private AutoResetOdometry (Swerve swerve) {
+            super(() -> swerve.resetPosition(getPosition(swerve, VisionHandler.getInstance())), swerve);
+        }
+        
+        private static Position getPosition (Swerve swerve, VisionHandler visionHandler) {
+            // Direction is still correct -- assume robot is pointed directly away from the hub (i.e. the intake is pointed
+            // away and the limelight is pointed straight at the hub). Use this knowledge plus distance from the hub to get
+            // position of the robot on the field 
+            final double direction = swerve.getPosition().getDirection();
+            
+            // Direction is negated because the limelight is facing AWAY from the front of the robot but TOWARD the hub
+            final Vector location = Vector.fromPolarDegrees(-direction, visionHandler.getDistanceFromHub());
+            
+            // Return the final position
+            return new Position(location, direction);
+        }
+        
+    }
+    
 }
